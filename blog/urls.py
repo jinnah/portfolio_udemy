3d754e57e070d5d43aaca0c671ce8d238deb1208
Mohammed Jinnah <jinnarmail@gmail.com>
2020-05-10 23:04:48 -0400

    Blog page is working for the first time

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path
+import blog.views
+
+
+
+
+urlpatterns = [
+    path('', blog.views.allblogs, name='allblogs' ),
+]
+
+
--- jobs/views.py
+++ jobs/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
+from.models import Job
+
+
 def home(request):
-    return render(request, 'jobs/home.html')
+    jobs = Job.objects #(This is to retrive the entire Job table from models.py. We need to import Job first (2nd line))
+    return render(request, 'jobs/home.html', {'jobs':jobs})
